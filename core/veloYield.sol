04bf5e0eb1a7c56c08b00281bd95ffec0756d2e9
Jabr0nie <sammyllc19@gmail.com>
2026-01-30 23:34:22 -0600

    update

---
+++ core/veloYield.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.7.6;
+
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+
+// Pool interfaces
+interface ICLPoolActions {
+    function swap(
+        address recipient,
+        bool zeroForOne,
+        int256 amountSpecified,
+        uint160 sqrtPriceLimitX96,
+        bytes calldata data
+    ) external returns (int256 amount0, int256 amount1);
+}
+
+interface ICLPoolState {
+    function slot0()
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            bool unlocked
+        );
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
+contract yieldData {
+
+    function getPoolList() external {
+        
+    }
+}
